e7118c37123c81ebbc471146b70046d130a91528
AlineCoutoDeSouza <alinecouto337@gmail.com>
2024-08-11 22:33:17 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; 
 import './App.css';
+import HelloWord from './components/HelloWord';
+
 
 function App() {
+
+const name = "Alininha"
+const newname = name.toUpperCase()
+
+function sum(a,b){
+  return a+b
+}
+
+const url  = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>Alterando o meu jsx</h2>
+    <p>Ola, {newname}</p>
+    <p>soma: {sum(2,2)}</p>
+    <img src={url} alt="Minha imagem" />
+    <HelloWord/>
     </div>
   );
 }
